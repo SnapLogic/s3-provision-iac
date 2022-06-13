4661f30809faa2d1407351ea15ea8fbc74b59469
Terry Tran <ttran@snaplogic.com>
2022-06-13 14:21:41 -0700

    asdf

--- main.tf
+++ main.tf
@@ -1,47 +1,14 @@
 provider "aws" {
-    region = var.region
+    region = "${local.region}"
 }
 
 locals {
     bucket_name = var.bucket_name
-    region = var.region
+    region = "${var.region}"
 }
 
-resource "aws_iam_role" "this" {
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-data "aws_iam_policy_document" "bucket_policy" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.this.assume_role_policy]
-    }
-
-    actions = [
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${local.bucket_name}",
-    ]
-  }
-}
-
+/* provision the bucket */
 resource "aws_s3_bucket" "test_bucket" {
-    bucket = local.bucket_name
+    bucket = "${local.bucket_name}"
 }
+
