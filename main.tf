71748c2d7cf28a4590cc8b9ffffa057af65a709a
Terry Tran <ttran@snaplogic.com>
2022-06-13 14:04:57 -0700

    added policies back in

--- main.tf
+++ main.tf
@@ -7,40 +7,40 @@ locals {
     region = var.region
 }
 
-# resource "aws_iam_role" "this" {
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "ec2.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role" "this" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
-# data "aws_iam_policy_document" "bucket_policy" {
-#   statement {
-#     principals {
-#       type        = "AWS"
-#       identifiers = [aws_iam_role.this.assume_role_policy]
-#     }
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.this.assume_role_policy]
+    }
 
-#     actions = [
-#       "s3:ListBucket",
-#     ]
+    actions = [
+      "s3:ListBucket",
+    ]
 
-#     resources = [
-#       "arn:aws:s3:::${local.bucket_name}",
-#     ]
-#   }
-# }
+    resources = [
+      "arn:aws:s3:::${local.bucket_name}",
+    ]
+  }
+}
 
 resource "aws_s3_bucket" "test_bucket" {
     bucket = local.bucket_name
